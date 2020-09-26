4d79802746fee38626012097d84dadb41e44f5d0
MrWebUzb <space.coding.programmer@gmail.com>
2020-09-26 10:32:06 +0500

    Updates

---
+++ precache-manifest.faf655b5bb165df98dc70a0ef93301a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45da847780d389f8bb94e59d951fa3c6",
+    "url": "/CodePenClone/index.html"
+  },
+  {
+    "revision": "c88d1e65b2773ad51ddb",
+    "url": "/CodePenClone/static/css/2.edb458bf.chunk.css"
+  },
+  {
+    "revision": "ca716c1947963189da55",
+    "url": "/CodePenClone/static/css/main.575ea778.chunk.css"
+  },
+  {
+    "revision": "c88d1e65b2773ad51ddb",
+    "url": "/CodePenClone/static/js/2.39617c20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodePenClone/static/js/2.39617c20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca716c1947963189da55",
+    "url": "/CodePenClone/static/js/main.fa9317bb.chunk.js"
+  },
+  {
+    "revision": "fc1c1151322d13a8f487",
+    "url": "/CodePenClone/static/js/runtime-main.e44751eb.js"
+  }
+]);
