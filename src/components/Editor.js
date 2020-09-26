08dadade29467a265251015bb9faf2418e87a2d3
MrWebUzb <space.coding.programmer@gmail.com>
2020-09-26 10:26:05 +0500

    Codepen clone

---
+++ src/components/Editor.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/css/css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/addon/hint/show-hint";
+import "codemirror/addon/hint/javascript-hint";
+import "codemirror/addon/hint/show-hint.css";
+import "codemirror/addon/hint/anyword-hint";
+import "codemirror/keymap/sublime";
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/fold/foldcode";
+import "codemirror/addon/fold/foldgutter";
+import "codemirror/addon/fold/brace-fold";
+import "codemirror/addon/fold/comment-fold";
+import "codemirror/addon/fold/foldgutter.css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
+
+const Editor = (props) => {
+  const { displayName, language, value, onChange } = props;
+
+  const [open, setOpen] = useState(true);
+
+  const handleChange = (editor, data, value) => {
+    onChange(value);
+  };
+  return (
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
+      <div className="editor-title">
+        {displayName}
+        <button
+          className="expand-collapse-btn"
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lint: true,
+          mode: language,
+          theme: "material",
+          lineWrapping: true,
+          smartIndent: true,
+          lineNumbers: true,
+          foldGutter: true,
+          gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+          autoCloseTags: true,
+          keyMap: "sublime",
+          matchBrackets: true,
+          autoCloseBrackets: true,
+          extraKeys: {
+            Tab: "autocomplete",
+          },
+        }}
+        editorDidMount={(editor) => {
+          setTimeout(() => {
+            editor.refresh();
+          }, 400);
+        }}
+      />
+    </div>
+  );
+};
+
+export default Editor;
